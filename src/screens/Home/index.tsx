51b1cc8b28b4ed21b661e2d57303139dcfae5f99
wcfx <wcfx.dev@gmail.com>
2021-07-23 00:28:47 -0300

    chore: projeto base com libs add

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Home: React.FC = () => (
+  <S.Container>
+    <S.Title>Hello World</S.Title>
+  </S.Container>
+);
+
+export default Home;
