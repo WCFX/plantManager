f8f4f7164e44718db103a5c7c564a7921a0c74f7
wcfx <wcfx.dev@gmail.com>
2021-07-23 18:47:22 -0300

    chore: add fonts

--- src/screens/Preload/index.tsx
+++ src/screens/Preload/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Text } from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
@@ -18,7 +19,7 @@ const Preload: React.FC = () => {
       if (token) {
         // Token
       } else {
-        navigation.navigate('Home');
+        // navigation.navigate('Home');
       }
     };
     setTimeout(() => {
@@ -35,7 +36,9 @@ const Preload: React.FC = () => {
         loop
         autoPlay
       />
-      <S.Title>Plant Manager</S.Title>
+      <S.Title>
+        <S.TitleColor>Plant</S.TitleColor>Manager
+      </S.Title>
     </S.Container>
   );
 };
